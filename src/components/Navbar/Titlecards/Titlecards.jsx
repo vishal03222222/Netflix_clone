7642a86ff536e54e5d8ead56043071142249c82e
vishal <mgvishalctr@gmail.com>
2025-03-24 11:46:28 +0530

    push

---
+++ src/components/Navbar/Titlecards/Titlecards.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Titlecards.css'
+const Titlecards = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Titlecards
---
+++ src/pages/Player/Player.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './Player.css'
+const Player = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Player
